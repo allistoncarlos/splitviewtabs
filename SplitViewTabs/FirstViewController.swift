626a552339356ba9b9927c73a8df4bcf7f7edb73
Alliston Aleixo <alliston@outlook.com>
2017-01-07 22:21:32 -0200

    Initial Commit

---
+++ SplitViewTabs/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SplitViewTabs
+//
+//  Created by Alliston Aleixo on 07/01/17.
+//  Copyright © 2017 Alliston Aleixo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
